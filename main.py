87e31a73a15c0e5ca6e7d05db0c841cd95eab05f
Hashim <109148083+5wHN28Dg@users.noreply.github.com>
2025-12-12 08:54:12 +0300

    added a button to search for peers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import dearpygui.dearpygui as dpg
 from dbus_fast import BusType, Variant
 from dbus_fast.aio import MessageBus
 
@@ -36,7 +37,6 @@ async def main():
         if await device_interface.get_device_type() == 30:
             print("Found WiFiP2P device, it is:", device, "you are good to go")
             break
-    await find_wifi_p2p_peers()
 
 
 async def find_wifi_p2p_peers():
@@ -44,7 +44,28 @@ async def find_wifi_p2p_peers():
         "org.freedesktop.NetworkManager.Device.WifiP2P"
     )
     await device_interface.call_start_find({})
-    print("Found WiFiP2P peers:", await device_interface.get_peers())
+    # print("Found WiFiP2P peers:", await device_interface.get_peers())
 
 
 asyncio.run(main())
+dpg.create_context()
+
+# add a font registry
+with dpg.font_registry():
+    # first argument ids the path to the .ttf or .otf file
+    default_font = dpg.add_font("font/ProggyVector-Regular.otf", 75)
+
+dpg.create_viewport(title="Direct Share", width=1280, height=720)
+
+with dpg.window(label="Example Window", no_title_bar=True, tag="fullscreen"):
+    dpg.bind_font(default_font)
+    dpg.add_text("Hello, world")
+    dpg.add_button(label="search for peers", callback=find_wifi_p2p_peers)
+    dpg.add_input_text(label="string", default_value="Quick brown fox")
+    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+dpg.setup_dearpygui()
+dpg.set_primary_window("fullscreen", True)
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
