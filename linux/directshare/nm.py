81001a487a1c8a05dd6b0151d6e52c45741fce4e
Hashim <109148083+5wHN28Dg@users.noreply.github.com>
2026-04-30 09:19:15 +0300

    Refactor NMClient to use async class
    Move the D-Bus connection and introspection logic into the `__init__`
    method of the `NMClient` class. This makes the client behave more like a
    standard asynchronous object, allowing for proper initialization and
    resource management.

--- linux/directshare/nm.py
+++ linux/directshare/nm.py
@@ -1,33 +1,34 @@
+from dbus_fast import BusType
+from dbus_fast.aio import MessageBus
+
+
 class NMClient:
-    async def main():
-        global bus
-        bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+    async def __init__(self):
+        self.bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
         # the introspection xml would normally be included in your project, but
         # this is convenient for development
-        introspection = await bus.introspect(
+        introspection = await self.bus.introspect(
             "org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager"
         )
 
-    service_obj = bus.get_proxy_object(
-        "org.freedesktop.NetworkManager",
-        "/org/freedesktop/NetworkManager",
-        introspection,
-    )
-    global interface
-    interface = service_obj.get_interface("org.freedesktop.NetworkManager")
-
-    devices = await interface.get_devices()
-
-    for device in devices:
-        global device_obj
-        device_obj = bus.get_proxy_object(
+        service_obj = self.bus.get_proxy_object(
             "org.freedesktop.NetworkManager",
-            device,
-            await bus.introspect("org.freedesktop.NetworkManager", device),
+            "/org/freedesktop/NetworkManager",
+            introspection,
         )
-        device_interface = device_obj.get_interface(
-            "org.freedesktop.NetworkManager.Device"
-        )
-        if await device_interface.get_device_type() == 30:
-            print("Found WiFiP2P device, it is:", device, "you are good to go")
-            break
+        interface = service_obj.get_interface("org.freedesktop.NetworkManager")
+
+        devices = await interface.get_devices()
+
+        for device in devices:
+            device_obj = self.bus.get_proxy_object(
+                "org.freedesktop.NetworkManager",
+                device,
+                await self.bus.introspect("org.freedesktop.NetworkManager", device),
+            )
+            device_interface = device_obj.get_interface(
+                "org.freedesktop.NetworkManager.Device"
+            )
+            if await device_interface.get_device_type() == 30:
+                print("Found WiFiP2P device, it is:", device, "you are good to go")
+                break
