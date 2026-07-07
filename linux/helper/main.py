be88296f665b4d611cf388bd553b41993fdb901d
Hashim <109148083+5wHN28Dg@users.noreply.github.com>
2026-07-07 16:50:01 +0300

    Add D-Bus backend initialization

--- linux/helper/main.py
+++ linux/helper/main.py
@@ -1,2 +1,13 @@
 # SPDX-FileCopyrightText: 2026 5wHN28Dg
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from dbus_fast import BusType
+from dbus_fast.aio import MessageBus
+
+
+class WpasBackend:
+    @classmethod
+    async def create(cls):
+        bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+        instance = cls(bus)
+        return instance
